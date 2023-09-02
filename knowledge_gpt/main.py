413bfaf540cfc925ddd139451d1a02e7285b3589
lukketsvane <iverfinne@gmail.com>
2023-09-02 12:43:51 +0200

    ye

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv
 import streamlit as st
-from fpdf import FPDF
-from reportlab.pdfgen import canvas
+import io
 from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import SimpleDocTemplate, Paragraph, ListFlowable, ListItem
 
 from knowledge_gpt.components.sidebar import sidebar
 from knowledge_gpt.ui import (
@@ -33,59 +35,24 @@ def on_send_inn(selected_schemes):
     print("Selected cultural support schemes:", selected_schemes)
 
 st.set_page_config(page_title="StøtteJungelen", page_icon="📖", layout="wide")
-
-css = '''
-<style>
-    [data-testid='stFileUploader'] button {
-        font-size: 0 !important;
-    }
-    [data-testid='stFileUploader'] button:after {
-        content: "Last opp filer";
-        font-size: medium !important;
-    }
-    [data-testid='stFileUploader'] div:last-child {
-        font-size: 0 !important;
-    }
-    [data-testid='stFileUploader'] div:last-child:after {
-        content: "Dra og slipp filer her"
-        font-size: small !important;
-    }
-</style>
-'''
+css = '''<style> /* CSS content here */ </style>'''
 st.markdown(css, unsafe_allow_html=True)
 
-if 'show_contact_info' not in st.session_state:
-    st.session_state['show_contact_info'] = False
-
 bootstrap_caching()
-
-if 'uploaded_files' not in st.session_state:
-    st.session_state['uploaded_files'] = []
 
 uploaded_files = sidebar(on_send_inn)
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 
 with st.form(key="qa_form"):
     col1, col2 = st.columns(2)
-    
-    with col1:
-        nested_col1, nested_col2 = st.columns([1, 1])
-        with nested_col1:
-            fornavn = st.text_input("Fornavn", "Steinar")
-        with nested_col2:
-            etternavn = st.text_input("Etternavn", "Raknes")
-        adresse = st.text_input("Adresse", "Bestumveien 5b")
-        nested_col3, nested_col4 = st.columns([1, 1])
-        with nested_col3:
-            postnummer = st.text_input("Postnummer", "0281")
-        with nested_col4:
-            poststed = st.text_input("Poststed", "Oslo")
-    
-    with col2:
-        prosjekttype = st.selectbox("prosjekttype", ["Musikk", "Annet"])
-        
-    query = st.text_area("Beskriv prosjektet ditt.")
-    submit = st.form_submit_button("Send inn", disabled=(uploaded_files is None or len(uploaded_files) == 0))
+    fornavn = col1.text_input("Fornavn", "Steinar")
+    etternavn = col1.text_input("Etternavn", "Raknes")
+    adresse = col1.text_input("Adresse", "Bestumveien 5b")
+    postnummer = col1.text_input("Postnummer", "0281")
+    poststed = col1.text_input("Poststed", "Oslo")
+    prosjekttype = col2.selectbox("prosjekttype", ["Musikk", "Annet"])
+    query = col2.text_area("Beskriv prosjektet ditt.")
+    submit = st.form_submit_button("Send inn")
 
 if submit:
     st.markdown(f"### Kontaktinformasjon:")
@@ -111,20 +78,14 @@ if uploaded_files:
     combined_file = CombinedFile(name="combined", id="combined_id", docs=combined_docs)
     chunked_file = chunk_file(combined_file, chunk_size=300, chunk_overlap=0)
 
-    with st.spinner("Indekserer dokumenter... Dette kan ta litt tid⏳"):
-        folder_index = embed_files(
-            files=[chunked_file],
-            embedding=EMBEDDING,
-            vector_store=VECTOR_STORE,
-            openai_api_key=openai_api_key,
-        )
+    folder_index = embed_files(
+        files=[chunked_file],
+        embedding=EMBEDDING,
+        vector_store=VECTOR_STORE,
+        openai_api_key=openai_api_key,
+    )
 
 if submit:
-    if not is_query_valid(query):
-        st.stop()
-
-    answer_col, sources_col = st.columns(2)
-
     result = query_folder(
         folder_index=folder_index,
         query=query,
@@ -133,20 +94,36 @@ if submit:
         temperature=0,
     )
 
-    with answer_col:
-        st.markdown("#### Utkast")
-        st.markdown(result.answer)
+    st.markdown("#### Utkast")
+    st.markdown(result.answer)
 
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("Arial", size=12)
-        pdf.multi_cell(0, 10, result.answer)
-        pdf_output = io.BytesIO()
-        pdf.output(pdf_output)
-
-        st.download_button(
-            label="Last ned PDF",
-            data=pdf_output.getvalue(),
-            file_name="resultat.pdf",
-            mime="application/pdf",
-        )
+    pdf_buffer = io.BytesIO()
+    pdf = SimpleDocTemplate(pdf_buffer, pagesize=letter)
+    
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='ListBullet', parent=styles['BodyText'], spaceAfter=20, bulletIndent=0, leftIndent=20, bulletText='-'))
+    
+    flowables = []
+    
+    for line in result.answer.split('\n'):
+        if line.startswith('# '):
+            flowables.append(Paragraph(line[2:], styles['Heading1']))
+        elif line.startswith('## '):
+            flowables.append(Paragraph(line[3:], styles['Heading2']))
+        elif line.startswith('### '):
+            flowables.append(Paragraph(line[4:], styles['Heading3']))
+        elif line.startswith('- '):
+            flowables.append(ListFlowable([ListItem(Paragraph(line[2:], styles['ListBullet']))]))
+        elif line.startswith('**'):
+            flowables.append(Paragraph(f"<strong>{line[2:-2]}</strong>", styles['BodyText']))
+        else:
+            flowables.append(Paragraph(line, styles['BodyText']))
+    
+    pdf.build(flowables)
+    
+    st.download_button(
+        label="Last ned PDF",
+        data=pdf_buffer.getvalue(),
+        file_name="resultat.pdf",
+        mime="application/pdf",
+    )
